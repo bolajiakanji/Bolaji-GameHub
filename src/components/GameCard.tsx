4bd161257e1e9cdbda81293f450a642defc0a290
lukman bolaji <bolajiakanji21@gmail.com>
2024-06-11 00:46:00 -0700

    Continue game card icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-    game: Game;
-
+  game: Game;
 }
-const GameCard = ({ game }: Props ) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card>
-        <Image src={game.background_image} />
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        {game.parent_platforms.map(({ platform }) => <PlatformIconList platforms={platform} />)}
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
