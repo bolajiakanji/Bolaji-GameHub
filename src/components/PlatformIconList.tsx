aca3119e16e2aa07ee72636150dfee4d30d03476
lukman bolaji <bolajiakanji21@gmail.com>
2024-06-11 23:26:02 -0700

    Displaying platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,9 +6,9 @@ import {
   FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import { MdPhoneIphone } from 'react-icons/md';
-import { SiNintendo } from 'react-icons/si';
-import { BsGlobe } from 'react-icons/bs';
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
@@ -17,25 +17,23 @@ interface Props {
   platforms: Platform;
 }
 
-const PlatformIconList = ({ platforms  }: Props) => {
-  const iconMap: { [key: string]: IconType } = { 
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
     nintendo: SiNintendo,
     mac: FaApple,
-    linux: FaLinux, 
+    linux: FaLinux,
     android: FaAndroid,
     ios: MdPhoneIphone,
-    web: BsGlobe
-  }
+    web: BsGlobe,
+  };
 
   return (
-    <HStack marginY={1}> 
-      
-        <Icon key={platforms.id} as={iconMap[platforms.slug]} color='gray.500'/>
     
-    </HStack>
+      <Icon  as={iconMap[platforms.slug]} color="gray.500" />
+    
   );
 };
 
