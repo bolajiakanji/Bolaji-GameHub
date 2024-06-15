16739aace8a6417f7ee03b1d443d832e0d00463a
lukman bolaji <bolajiakanji21@gmail.com>
2024-06-15 00:47:34 -0700

    Highlight the selected genre git push" to publish your local commits)

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={handleSelctedGenre} />
+          <GenreList onSelectGenre={handleSelctedGenre} selectedGenre={selectedGenre} />
         </GridItem>
       </Show>
       <GridItem area="main">
