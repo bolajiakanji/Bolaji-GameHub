e7fdcb175c8bba10bba0ba97cd904481aa3fd511
lukman bolaji <bolajiakanji21@gmail.com>
2024-07-26 16:11:15 -0700

    Build an error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import GameDetailPage from "./pages/GameDetailPage";
+import Error from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/', element: <Layout />,
+        errorElement: <Error />,
         children: [
             { index: true, element: <Home /> },
-            { path: 'games/:id', element: <GameDetailPage />}
+            { path: 'games/:id', element: <GameDetailPage /> },
+            
+            
         ]
+    
+        
 
         
 
