dfd9101689a09b9cf69a7535a1990a4f5883321b
lukman bolaji <bolajiakanji21@gmail.com>
2024-07-27 20:52:51 -0700

    Refactor- move entity interface

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../hooks/useGames";
+import  Game  from "../entities/Game";
 import { Card, CardBody, HStack, Heading, Image, Flex } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
@@ -6,33 +6,36 @@ import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
 
-
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Link to={'/games/' + game.slug} >
-      <Card _hover={{
-        transform: 'scale(1.o5)',
-        transition: 'transform .15s ease-in'
-    }} borderRadius={10} overflow='hidden'>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Flex justifyContent="space-between" marginBottom={3}>
-          <HStack>
-            {game.parent_platforms.map(({ platform }) => (
-              <PlatformIconList key={platform.slug} platforms={platform} />
-            ))}
-          </HStack>
-          <CriticScore score={game.metacritic} />
-        </Flex>
-        <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
+    <Link to={"/games/" + game.slug}>
+      <Card
+        _hover={{
+          transform: "scale(1.o5)",
+          transition: "transform .15s ease-in",
+        }}
+        borderRadius={10}
+        overflow="hidden"
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <Flex justifyContent="space-between" marginBottom={3}>
+            <HStack>
+              {game.parent_platforms.map(({ platform }) => (
+                <PlatformIconList key={platform.slug} platforms={platform} />
+              ))}
+            </HStack>
+            <CriticScore score={game.metacritic} />
+          </Flex>
+          <Heading fontSize="2xl">
+            {game.name} <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
       </Card>
-      </Link>
+    </Link>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,21 +1,10 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../store"
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import ms from "ms";
+import  Game  from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  background_image: string;
-  description_raw: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-}
 
 const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game>, Error>({
